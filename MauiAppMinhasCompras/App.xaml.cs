261fe00df72fc2ee595639f008f193820169ccfa
zeguilherme1000 <guitar.rock221@gmail.com>
2026-03-14 14:17:42 -0300

    Adição Model e troca da tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -11,7 +11,9 @@ namespace MauiAppMinhasCompras
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            //return new Window(new AppShell());
+
+            return new Window(new NavigationPage(new Views.ListaProduto()));
         }
     }
 }
--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade  { get; set; }
+        public double Preco  { get; set; }
     }
 }

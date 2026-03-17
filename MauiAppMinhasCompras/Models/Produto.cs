9e0542f53cb9c6feac55b29c9ba9a9ca73650770
zeguilherme1000 <guitar.rock221@gmail.com>
2026-03-17 17:35:42 -0300

    Novas funcionalidades

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,5 +9,6 @@ namespace MauiAppMinhasCompras.Models
         public string Descricao { get; set; }
         public double Quantidade  { get; set; }
         public double Preco  { get; set; }
+        public double Total { get => Quantidade * Preco; }
     }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,23 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
+    private List<Produto> _listaCompletaNaMemoria = new List<Produto>();
+
+    public ListaProduto()
 	{
 		InitializeComponent();
-	}
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        _listaCompletaNaMemoria = await App.Db.GetAll();
+        lst_produtos.ItemsSource = _listaCompletaNaMemoria;
+    }
 
     private void ToolbarItem_Adicionar(object sender, EventArgs e)
     {
@@ -17,5 +29,70 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlertAsync("Ops", ex.Message, "OK");
 		}
+    }
+
+    private async void MenuItem_Remover(object sender, EventArgs e)
+    {
+		try
+		{
+			var idProduto = (int)((MenuItem)sender).CommandParameter;
+
+            await App.Db.Delete(idProduto);
+            await CarregarProdutos();
+
+            await DisplayAlertAsync("Sucesso", "Item removido.", "OK");
+        }
+		catch (Exception ex)
+		{
+            await DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string textoDigitado = e.NewTextValue.ToLower();
+
+            if (string.IsNullOrWhiteSpace(textoDigitado))
+            {
+                lst_produtos.ItemsSource = _listaCompletaNaMemoria;
+            }
+            else
+            {
+                var listaFiltrada = _listaCompletaNaMemoria
+                            .Where(produto => produto.Descricao.ToLower().Contains(textoDigitado))
+                            .ToList();
+
+                lst_produtos.ItemsSource = listaFiltrada;
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
+        
+    }
+
+    private async Task CarregarProdutos()
+    {
+        lst_produtos.ItemsSource = await App.Db.GetAll();
+    }
+
+    private async void ToolbarItem_Somar(object sender, EventArgs e)
+    {
+        try
+        {
+            double soma = ((List<Produto>)lst_produtos.ItemsSource).Sum(p => p.Total);
+
+            string mensagem = $"O total é {soma:C}";
+
+            await DisplayAlertAsync("Total dos produtos", mensagem, "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
+        
     }
 }

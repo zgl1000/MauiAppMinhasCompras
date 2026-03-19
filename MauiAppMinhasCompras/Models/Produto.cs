a45e38f3973f5cb97515c39a50bc1309c3752ac9
zeguilherme1000 <guitar.rock221@gmail.com>
2026-03-18 23:33:35 -0300

    Validações Model produto

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,10 +4,35 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade  { get; set; }
+        public string Descricao 
+        { 
+            get => _descricao;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade  
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("A quantidade deve ser maior que zero");
+                }
+            } 
+        }
         public double Preco  { get; set; }
         public double Total { get => Quantidade * Preco; }
     }
